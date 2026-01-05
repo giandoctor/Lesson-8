fc04a139c8eff14f3e54cafd27463858be632eae
giandoctor <gian.doctor@hotmail.com>
2026-01-05 15:58:42 +1100

    Divisible number

---
+++ Divisible.py
@@ -0,0 +1,7 @@
+Numerator=int(input("Enter a number:"))
+Denomitor=int(input("Enter a number:"))
+cal=Numerator%Denomitor==0
+if cal:
+    print("It is divisible")
+else:
+    print("It is not divisible")
